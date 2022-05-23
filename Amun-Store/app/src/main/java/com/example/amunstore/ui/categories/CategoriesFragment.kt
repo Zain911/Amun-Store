604a492eb2a1fcb42267eb3f9d00496cf1026c99
Eslam Esmael <EslamEsmael012@Gmail.com>
2022-05-23 15:11:53 +0200

    Categories list with Recycler and its adapter, observe on the list from the view model

--- Amun-Store/app/src/main/java/com/example/amunstore/ui/categories/CategoriesFragment.kt
+++ Amun-Store/app/src/main/java/com/example/amunstore/ui/categories/CategoriesFragment.kt
@@ -7,28 +7,54 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.example.amunstore.R
 import com.example.amunstore.databinding.FragmentCategoriesBinding
+import com.example.amunstore.model.Products
 import com.google.android.material.tabs.TabLayout
 
 class CategoriesFragment : Fragment() {
 
+
+    private val viewModel: CategoriesViewModel by viewModels()
+
     private var _binding: FragmentCategoriesBinding? = null
+
     private val binding get() = _binding!!
 
+    private lateinit var productsAdapter: CategoriesProductAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(CategoriesViewModel::class.java)
         _binding = FragmentCategoriesBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        productsAdapter = CategoriesProductAdapter(arrayListOf())
+        binding.categoriesProductsRecyclerView.layoutManager = GridLayoutManager(context, 3)
+        binding.categoriesProductsRecyclerView.adapter = productsAdapter
+
+
+        //TODO remove the list it is just for testing and viewing products in the screen
+        val mutableList = mutableListOf<Products>()
+        mutableList.add(Products(title = "NewShirt"))
+        mutableList.add(Products(title = "NewShirt"))
+        mutableList.add(Products(title = "NewShirt"))
+        mutableList.add(Products(title = "NewShirt"))
+        mutableList.add(Products(title = "NewShirt"))
+        mutableList.add(Products(title = "NewShirt"))
+        productsAdapter.productList = mutableList
+
+        viewModel.productList.observe(viewLifecycleOwner) {
+            productsAdapter.productList = it as MutableList<Products>
+        }
 
         return root
     }
