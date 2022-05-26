e90f11d6dcb1ee32a6c3bdcaa6e3258ff19afbb5
Abdalla badr <Bossmail28@gmail.com>
2022-05-26 16:44:30 +0200

    updated commit as review said ,deleted un necessary models & updated repo beside deleteing previously created repo , modified viewmodel ,imagesview pager

--- Amun-Store/app/src/main/java/com/example/amunstore/ui/details/ProductDetailsFragment.kt
+++ Amun-Store/app/src/main/java/com/example/amunstore/ui/details/ProductDetailsFragment.kt
@@ -11,33 +11,32 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.viewpager2.widget.ViewPager2
-
 import com.example.amunstore.R
 import com.example.amunstore.databinding.FragmentProductDetailsBinding
-import com.example.amunstore.model.getImage
-import com.example.amunstore.ui.home.SliderViewPagerAdapter
+import com.example.amunstore.model.product.Images
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Job
 
 @AndroidEntryPoint
 class ProductDetailsFragment(val productID: Long) : Fragment() {
 
-
     private val viewModel: ProductDetailsViewModel by viewModels()
     private var _binding: FragmentProductDetailsBinding? = null
     private val binding get() = _binding!!
 
     //viewPager components
+    private lateinit var  productImagesList : List<Images>
     private lateinit var job: Job
     private lateinit var viewPager: ViewPager2
-    private lateinit var viewPagerAdapter: SliderViewPagerAdapter
-    private lateinit var dots: Array<TextView?>
+    private lateinit var viewPagerAdapter: DetailsSliderViewPagerAdapter
+    private lateinit var dots : Array<TextView?>
     private var onImageSliderChange = object : ViewPager2.OnPageChangeCallback() {
         override fun onPageSelected(position: Int) {
             super.onPageSelected(position)
             addDots(position)
         }
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -46,31 +45,42 @@ class ProductDetailsFragment(val productID: Long) : Fragment() {
     ): View {
         _binding = FragmentProductDetailsBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        viewPager = binding.productImageView
+        viewModel.getProductDetails(productID)
 
         viewModel.productDetails.observe(viewLifecycleOwner) {
-
             if (it != null) {
-                binding.productTitleText.text = it?.products?.title
-                binding.productVendorText.text = it?.products?.vendor
-                binding.productPriceText.text= it.products.variants[0].price
-                binding.productBodyHtmlText.text = it?.products?.bodyHtml
-                binding.productStatusText.text = it?.products?.status   // need some equipments
+                binding.productTitleText.text = it.product.title
+                binding.productVendorText.text = it.product.vendor
+                binding.productPriceText.text = it.product.variants[0].price
+                binding.productBodyHtmlText.text = it.product.bodyHtml
+                binding.productStatusText.text = it.product.status   // need some equipments
+
+                //inflating view pager and it's dot from the respond of the api
+                productImagesList = it.product.images
+                dots =arrayOfNulls<TextView>(productImagesList.size)
+                viewPagerAdapter = DetailsSliderViewPagerAdapter(productImagesList)
+                viewPager.apply {
+                    adapter = viewPagerAdapter
+                    registerOnPageChangeCallback(onImageSliderChange)
+
+                }
             }
         }
 
-            viewModel.errorMessage.observe(viewLifecycleOwner) {
-                Toast.makeText(context, it, Toast.LENGTH_LONG).show()
+        viewModel.errorMessage.observe(viewLifecycleOwner) {
+            Toast.makeText(context, it, Toast.LENGTH_LONG).show()
 
-            }
+        }
 
-            viewModel.getProductDetails(productID)
+
 
         return root
     }
 
     private fun addDots(currentImage: Int) {
         binding.productLinearLayoutDots.removeAllViews()
-        for (i in getImage().indices) {
+        for (i in productImagesList.indices) {
             dots[i] = TextView(context)
             dots[i]?.text = Html.fromHtml("&#8226", Html.FROM_HTML_MODE_LEGACY)
             dots[i]?.textSize = 32f
@@ -90,7 +100,6 @@ class ProductDetailsFragment(val productID: Long) : Fragment() {
         super.onPause()
         viewPager.unregisterOnPageChangeCallback(onImageSliderChange)
     }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
