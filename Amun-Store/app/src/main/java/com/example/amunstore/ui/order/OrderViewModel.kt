d72bf40ec74b01b75a9249cb2f523ada38612c8a
Walid Zain <walidzeno@gmail.com>
2022-06-05 12:21:55 +0200

    ADD UI of order screen and UI of add new address

---
+++ Amun-Store/app/src/main/java/com/example/amunstore/ui/order/OrderViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.amunstore.ui.order
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class OrderViewModel : ViewModel() {
+
+
+}
