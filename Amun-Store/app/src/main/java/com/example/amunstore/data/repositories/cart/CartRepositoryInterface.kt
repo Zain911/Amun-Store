32f95faae4b5744e7ee4c447f2e9309f4ed1a692
Walid Zain <walidzeno@gmail.com>
2022-06-05 20:32:06 +0200

    Implement Dao ,Room and set product fom product details  to cart fragment and retrieve from Room

---
+++ Amun-Store/app/src/main/java/com/example/amunstore/data/repositories/cart/CartRepositoryInterface.kt
@@ -0,0 +1,11 @@
+package com.example.amunstore.data.repositories.cart
+
+import androidx.lifecycle.LiveData
+import com.example.amunstore.data.model.cart.ItemCart
+
+
+interface CartRepositoryInterface {
+    suspend fun getAllItemCart(): LiveData<List<ItemCart>>
+    suspend fun addItem(itemCart: ItemCart)
+    fun deleteItem(itemCart: ItemCart)
+}
---
+++ Amun-Store/app/src/main/java/com/example/amunstore/ui/cart/CartViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.amunstore.ui.cart
+
+import androidx.lifecycle.*
+import com.example.amunstore.data.model.cart.ItemCart
+import com.example.amunstore.data.repositories.cart.CartRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CartViewModel @Inject constructor(val cartRepository: CartRepository) : ViewModel() {
+
+    var cartItems = MutableLiveData<List<ItemCart>>()
+
+    //  var favList = MutableLiveData<List<Product>>()
+    var data: LiveData<List<ItemCart>> = cartItems
+
+    val x = Observer<List<ItemCart>> {
+        cartItems.postValue(it)
+    }
+
+
+    suspend fun getCartItems() {
+        cartRepository.getAllItemCart().observeForever(x)
+        //cartItems.postValue(cartRepository.getAllItemCart())
+    }
+
+
+    override fun onCleared() {
+        super.onCleared()
+        cartItems.removeObserver(x)
+    }
+
+    suspend fun removeItem(itemCart: ItemCart) {
+        cartRepository.deleteItem(itemCart)
+
+    }
+}
