6c3d297deaa275f26df94ffe4e9f38dd030efc79
TheBoss000 <Bossmail28@gmail.com>
2022-06-07 16:12:27 +0200

    forogt to delete //comment

--- Amun-Store/app/src/main/java/com/example/amunstore/ui/auth/login/SigninFragment.kt
+++ Amun-Store/app/src/main/java/com/example/amunstore/ui/auth/login/SigninFragment.kt
@@ -1,26 +1,17 @@
 package com.example.amunstore.ui.auth.login
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewTreeLifecycleOwner
 import androidx.navigation.ActionOnlyNavDirections
-import androidx.navigation.fragment.FragmentNavigator
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import com.example.amunstore.MainActivity
 import com.example.amunstore.R
 import com.example.amunstore.databinding.FragmentLoginBinding
 import com.example.amunstore.ui.auth.AuthViewModel
-import com.example.amunstore.ui.favourites.FavouritesViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.internal.managers.FragmentComponentManager
 
 @AndroidEntryPoint
 class SigninFragment : Fragment() {
@@ -33,7 +24,7 @@ class SigninFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
@@ -43,15 +34,8 @@ class SigninFragment : Fragment() {
             ActionOnlyNavDirections(R.id.action_loginFragment_to_registerFragment)
         ) }
 
-
         return root
     }
-
-//    private fun tryAgain(message:String) {
-//        Toast.makeText(context,message , Toast.LENGTH_LONG).show()
-//        loginBtn.visibility = View.VISIBLE
-//    }
-
 
     private fun showBottomSheetDialogFragment() {
         val bottomSheetFragment = LoginBottomSheetDialogFragment(viewModel)
