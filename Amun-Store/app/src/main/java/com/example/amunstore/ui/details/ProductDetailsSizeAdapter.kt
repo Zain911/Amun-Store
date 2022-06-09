a6e01c01c3272342045fe29edb841a576d76753f
Abdalla badr <Bossmail28@gmail.com>
2022-06-09 20:09:15 +0200

    upgrade details fragment_product_details.xml to be updated when choose size from size elemnts, also updates in UI (#52)

--- Amun-Store/app/src/main/java/com/example/amunstore/ui/details/ProductDetailsSizeAdapter.kt
+++ Amun-Store/app/src/main/java/com/example/amunstore/ui/details/ProductDetailsSizeAdapter.kt
@@ -1,28 +1,20 @@
 package com.example.amunstore.ui.details
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
 import android.widget.CheckedTextView
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.core.widget.CheckedTextViewCompat
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.amunstore.R
-import com.example.amunstore.data.model.details.ProductDetailsResponse
-import com.example.amunstore.data.model.product.Options
 
 
 class ProductDetailsSizeAdapter(
     private val arrayList: ArrayList<String>,
     val clickItem: (String) -> Unit,
 ) : RecyclerView.Adapter<ProductDetailsSizeAdapter.ViewHolder>() {
-
-
-    var checkedItemPosition = 0
+    var checkedItemPosition = MutableLiveData(0)
     lateinit var item: String
 
     // create new views
@@ -36,21 +28,18 @@ class ProductDetailsSizeAdapter(
     }
 
     // binds the list items to a view
+    @SuppressLint("NotifyDataSetChanged")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        if (position == checkedItemPosition) {
-            holder.size.isChecked = true
-        } else {
-            holder.size.isChecked = false
-        }
+        holder.size.isChecked = position == checkedItemPosition.value
 
         holder.size.text = arrayList[position]
         holder.size.setOnClickListener {
 
-            if (checkedItemPosition != position) {
+            if (checkedItemPosition.value != position) {
                 holder.size.isChecked = true
-                checkedItemPosition = position
+                checkedItemPosition.value = position
+                checkedItemPosition.postValue(position)
                 clickItem(arrayList[position])
-
             }
             //update fragment
             notifyDataSetChanged()
