5bdeda7c80bfa159039dd9f4463b03eece700fa1
Walid Zain <walidzeno@gmail.com>
2022-06-16 21:56:19 +0200

    Handel max and min of quantity of product on cart based in availability in inventory

--- Amun-Store/app/src/main/java/com/example/amunstore/ui/cart/CartViewModel.kt
+++ Amun-Store/app/src/main/java/com/example/amunstore/ui/cart/CartViewModel.kt
@@ -31,13 +31,15 @@ class CartViewModel @Inject constructor(private val cartRepository: CartReposito
     }
 
     fun increaseItemQuantity(itemCart: ItemCart) {
-        cartRepository.updateItem(itemCart.apply { item_number = item_number?.plus(1) })
+        if ((itemCart.maxItem ?: 0) >= (itemCart.item_number ?: 0))
+            cartRepository.updateItem(itemCart.apply { item_number = item_number?.plus(1) })
     }
 
     fun decreaseItemQuantity(itemCart: ItemCart) {
-        cartRepository.updateItem(itemCart.apply {
-            item_number = item_number?.minus(1)
-        })
+        if (itemCart.item_number !in 0..1)
+            cartRepository.updateItem(itemCart.apply {
+                item_number = item_number?.minus(1)
+            })
     }
 
     // increase item
