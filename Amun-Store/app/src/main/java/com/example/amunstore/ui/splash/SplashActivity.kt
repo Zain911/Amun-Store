bc037dbd6c99e6aa9c77581e0c897cc364d4b788
Walid Zain <walidzeno@gmail.com>
2022-06-27 14:18:20 +0200

    ADD Splash Screen with animation and app icon.

---
+++ Amun-Store/app/src/main/java/com/example/amunstore/ui/splash/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.example.shopy.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.example.amunstore.R
+import com.example.amunstore.ui.intro.IntroActivity
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val motionLayout = findViewById<MotionLayout>(R.id.motionLayout)
+        motionLayout.addTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                startActivity(Intent(this@SplashActivity, IntroActivity::class.java))
+                finish()
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+            }
+
+        })
+    }
+}
