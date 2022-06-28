66fa542857ca8c0eaa0d9d06dac9f353c83da1ad
Walid Zain <walidzeno@gmail.com>
2022-06-28 14:56:45 +0200

    apply discuss notes

--- Amun-Store/app/src/main/java/com/example/amunstore/data/model/settings/FAQModel.kt
+++ Amun-Store/app/src/main/java/com/example/amunstore/data/model/settings/FAQModel.kt
@@ -7,10 +7,10 @@ data class FAQModel(
 
 var listFQA = listOf(
     FAQModel("Is this app available on in Egypt?", "Yes, it's exclusive in Egypt."),
-    FAQModel("How much are the delivery fees?", "It's based on your location."),
+    FAQModel("What is limit to make order?", "there is no limit."),
     FAQModel("What are the payment methods?", "You can pay either online or on delivery."),
     FAQModel(
         "What is the return policy?",
-        "You have 14 days to return the product but it needs to a brand-new, or 30 days if there is any defect in it."
+        "Based on vendor policy you can check in product details."
     )
 )
