20b044b463d6664a928c4cad12cf474c83ee6124
Abdalla badr <Bossmail28@gmail.com>
2022-06-29 21:43:44 +0200

    created zoomable WebView inside dialog in product details (#121)

--- Amun-Store/app/src/main/java/com/example/amunstore/ui/details/DetailsSliderViewPagerAdapter.kt
+++ Amun-Store/app/src/main/java/com/example/amunstore/ui/details/DetailsSliderViewPagerAdapter.kt
@@ -1,8 +1,9 @@
 package com.example.amunstore.ui.details
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.app.Dialog
+import android.content.Context
+import android.view.*
+import android.webkit.WebView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.amunstore.R
@@ -17,9 +18,29 @@ class DetailsSliderViewPagerAdapter(private val imageList: List<Images>) :
         private val slider = itemView.findViewById<ShapeableImageView>(R.id.sliderImageView)
 
         fun bind(data: String?) {
-         Glide.with(slider)
+            Glide.with(slider)
                 .load(data)
                 .into(slider)
+            slider.setOnClickListener { showZoomableImage(slider.context, data.toString()) }
+        }
+
+        private fun showZoomableImage(context: Context, fileUrl: String) {
+            val d = Dialog(context, android.R.style.Theme_DeviceDefault_Light_NoActionBar)
+            d.window?.setGravity(Gravity.CENTER)
+            d.setCancelable(true)
+            val wv = WebView(context)
+            wv.layoutParams = WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.MATCH_PARENT
+            )
+            wv.loadUrl(fileUrl)
+            wv.settings.builtInZoomControls = true
+            wv.settings.setSupportZoom(true)
+            wv.settings.loadWithOverviewMode = true
+            wv.settings.useWideViewPort = true
+            wv.zoomOut()
+            d.setContentView(wv)
+            d.show()
         }
     }
 
